7301e239ec5794eb39ff8986997a4f47d80c504f
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-05-27 15:47:26 +0200

    AZ-499: Handle var.subscription_ids is null

--- resources.tf
+++ resources.tf
@@ -5,9 +5,9 @@ resource "azurerm_management_group" "mgmt_group" {
   display_name               = local.mg_name
   parent_management_group_id = var.parent_management_group_id
 
-  name     = var.management_group_internal_name
+  name = var.management_group_internal_name
 
-  subscription_ids = coalescelist(var.subscription_ids, [
+  subscription_ids = var.subscription_ids == null ? null : coalescelist(var.subscription_ids, [
     data.azurerm_subscription.current.subscription_id,
   ])
 }
