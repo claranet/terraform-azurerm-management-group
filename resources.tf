b2280497314181ee2c0cc88aade223e905b6e543
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-05-28 17:34:07 +0200

    AZ-499: Use a data to retrieve the full ID path for Subscriptions

--- resources.tf
+++ resources.tf
@@ -8,9 +8,15 @@ resource "azurerm_management_group" "mgmt_group" {
   name = var.management_group_internal_name
 }
 
+data "azurerm_subscription" "sub" {
+  for_each = toset(local.subscription_ids)
+
+  subscription_id = each.key
+}
+
 resource "azurerm_management_group_subscription_association" "mg-sub" {
   for_each = toset(local.subscription_ids)
 
   management_group_id = azurerm_management_group.mgmt_group.id
-  subscription_id     = each.key
+  subscription_id     = data.azurerm_subscription.sub[each.key].id
 }
