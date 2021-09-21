f29a1327747532f0918d2aca8064d50b2b7ac106
Rémi Beaufils <remi.beaufils@fr.clara.net>
2021-09-21 11:32:40 +0200

    AZ-572: Revamp examples + improve CI

---
+++ examples/main/variables.tf
@@ -0,0 +1,9 @@
+variable "client_name" {
+  description = "Client name/account used in naming"
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name"
+  type        = string
+}
